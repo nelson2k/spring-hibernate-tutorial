b8e6f4fc92b1b5e89b99b5671cc5482771d9ef7b
Nelson DaSilva <nelson2k@gmail.com>
2022-07-15 20:08:45 -0300

    initial commit

---
+++ src/main/java/com/elfeedo/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.elfeedo.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practive";
+	}
+}
---
+++ src/main/java/com/elfeedo/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.elfeedo.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+}
